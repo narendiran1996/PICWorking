c360be600359091a5432e05f98ac1869fc5b3c5b
narendiran1996 <narendiran1996@gmail.com>
2021-07-01 07:44:04 +0530

    added led blinking with timer 0 interrupt

---
+++ Programs/BlinkLedWithTimer.c
@@ -0,0 +1,56 @@
+// BEGIN CONFIG
+#pragma config OSC = HS   // Oscillator Selection (HS oscillator)
+#pragma config OSCS = ON  // Oscillator Switch Disbale
+#pragma config PWRT = OFF // Power UpTime Disbale (PWRT disabled)
+#pragma config BOR = ON   // Brown-out Reset (BOR enabled)
+#pragma config WDT = OFF  // Watch Dog timer disbaled
+#pragma config LVP = OFF  // Low-Voltage (Single-Supply) In-Circuit Serial Programming Enable bit (RB3 is digital I/O, HV on MCLR must be used for programming)
+//END CONFIG
+
+#define _XTAL_FREQ 4000000L
+
+#include <xc.h>
+
+unsigned char flag = 0;
+
+void __interrupt() timer0Interrupt(void) // interrupt function
+{
+    if ((INTCON & (1 << 2)) != 0)
+    {
+
+        LATA = ((LATA & 0x01) == 0) ? (LATA | 0x01) : (LATA & 0xFE);
+        TMR0L = 100;         // load with the value of 256-TMR0L counts to get
+        INTCON &= ~(1 << 2); // clear timer interupt flag
+        INTCON |= (1 << 5);  // enable timer intterupt
+    }
+}
+void timer0Init()
+{
+    TMR0L = 100;        // load with the value of 256-TMR0L counts to get
+    T0CON |= (1 << 6);  // Timer 0 as 8-bit counting
+    T0CON &= ~(1 << 5); // Timer 0 clock source as internal clk (CLKO)
+
+    // presclar of 32
+    T0CON |= (1 << 2);
+    T0CON &= ~(1 << 1);
+    T0CON &= ~(1 << 0);
+
+    T0CON &= ~(1 << 4); // presclar assigned
+
+    INTCON |= (1 << 5); // Timer0 interrupt enable
+    INTCON |= (1 << 7); // Global Interrupt enable
+
+    T0CON |= (1 << 7); // start the time0
+}
+
+void main()
+{
+    TRISA = TRISA & ~(1 << 0);
+    LATA = LATA & ~(1 << 0);
+
+    timer0Init();
+
+    while (1)
+    {
+    }
+}
--- README.md
+++ README.md
@@ -2,10 +2,16 @@
 
 Learning PIC18F452 Microcontroller for usage of Advanced Peripherals  (mainly USB).
 
+## Details
+- **Compiler :** xc8
+- **Simulator :** Proteus
+
+
 
 ## Basic Programs
 1) Led Blinking
 2) Led Chasing
 3) Led Dice
-4) Multiplexing Between Two Seven Segment Display
+4) Blink Led Using Timer Interrupts
+5) Multiplexing Between Two Seven Segment Display
     - Without Time (only static display of numbers)
